5da4ec2fc0bec656a95865c7886b37fdecc6b901
Shreeya Paudel <shreeya.paudel.8e@gmail.com>
2025-02-16 12:13:48 +0545

    Question 2 b

---
+++ QuetionNo2/Question2b.java
@@ -0,0 +1,8 @@
+package QuetionNo2;
+
+//You have two points in a 2D plane, represented by the arrays x_coords and y_coords. The goal is to find
+//the lexicographically pair i.e. (i, j) of points (one from each array) that are closest to each other
+
+public class Question2b {
+    
+}
