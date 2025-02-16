a8906633ce3ba2b7ca15cda7aea4772ca07a409a
Shreeya Paudel <shreeya.paudel.8e@gmail.com>
2025-02-16 07:59:07 +0545

    Question No 2 a

---
+++ QuetionNo2/Question2a.java
@@ -0,0 +1,13 @@
+
+package QuetionNo2;
+
+import java.util.*;
+
+
+//You have a team of n employees, and each employee is assigned a performance rating given in the
+//integer array ratings. You want to assign rewards to these employees based on the following rules:
+//Every employee must receive at least one reward.
+//Employees with a higher rating must receive more rewards than their adjacent colleagues.
+//Goal:
+//Determine the minimum number of rewards you need to distribute to the employees.
+
